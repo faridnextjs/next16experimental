8b60c7482216c724e0a7747625ad00e5171ce7e3
illusion <17003685+faridnextjs@users.noreply.github.com>
2025-12-06 22:22:21 +0100

    first commit

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  reactCompiler: true,
+  cacheComponents: true,
+  crossOrigin: "anonymous",
+
+  experimental: {
+    browserDebugInfoInTerminal: true,
+    cssChunking: true, // default
+    turbopackFileSystemCacheForDev: true,
+    turbopackFileSystemCacheForBuild: true,
+  },
 };
 
 export default nextConfig;
