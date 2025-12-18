96618767557106d805bf9aea2ec0a2bd573dc027
Farid Ganbarov <17003685+faridnextjs@users.noreply.github.com>
2025-12-18 22:15:19 +0100

    started Next.js 16 Ultimate course

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,10 +3,12 @@ import TestClient from "@/components/testClient";
 export default function Home() {
   console.log("I am Server Component");
   return (
-    <>
-      {" "}
-      Page
+    <main>
+      <h1 className="text-2xl text-red-500">
+        {" "}
+        Welcome to the Ultimate Next.js 16 (Experimental) Showdown
+      </h1>
       <TestClient />
-    </>
+    </main>
   );
 }
