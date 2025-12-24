42d45d8c13b4d7a17e3d616e47d7d828d7596aaf
Farid Ganbarov <17003685+faridnextjs@users.noreply.github.com>
2025-12-24 23:20:38 +0100

    added localFont

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,23 @@
 import TestClient from "@/components/testClient";
+import localFont from "next/font/local";
+
+const spaceGrotesk = localFont({
+  src: "../fonts/SpaceGrotesk.ttf",
+  variable: "--font-space-grotesk",
+  display: "swap",
+  weight: "300 400 500 600 700",
+});
 
 export default function Home() {
   console.log("I am Server Component");
+
   return (
     <>
-      <h1>Welcome to the Ultimate Next.js 16 (Experimental) Showdown</h1>
+      <h1 className={`${spaceGrotesk.className}`}>
+        Welcome to the Ultimate Next.js 16 (Experimental) Showdown (Space
+        Grotesk)
+      </h1>
+      <p className="text-violet-500">Google Sans Flex (Google Sans Flex)</p>
       <TestClient />
     </>
   );
